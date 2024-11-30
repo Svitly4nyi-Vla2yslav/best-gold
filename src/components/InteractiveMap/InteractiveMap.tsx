28a17a245cb9fd2fecff526d2fe20142204585f3
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-11-30 18:45:49 +0100

    leaflet

--- src/components/InteractiveMap/InteractiveMap.tsx
+++ src/components/InteractiveMap/InteractiveMap.tsx
@@ -5,53 +5,53 @@ import 'leaflet/dist/leaflet.css';
 import { locations } from './locations';
 
 const goldIcon = new L.Icon({
-    iconUrl: 'https://png.pngtree.com/png-vector/20220108/ourmid/pngtree-three-dimensional-metal-texture-gold-coin-icon-png-image_4122362.png', // Замініть на URL вашої іконки
-    iconSize: [30, 30],
-  });
+  iconUrl:
+    'https://png.pngtree.com/png-vector/20220108/ourmid/pngtree-three-dimensional-metal-texture-gold-coin-icon-png-image_4122362.png', // Замініть на URL вашої іконки
+  iconSize: [30, 30],
+});
 
-  interface Location {
-    id: number;
-    name: string;
-    address: string;
-    position: [number, number]; // координати для Marker
-  }
-
+interface Location {
+  id: number;
+  name: string;
+  address: string;
+  position: [number, number]; // координати для Marker
+}
 
 const InteractiveMap: React.FC = () => {
-    const [activeLocation, setActiveLocation] = useState<Location | null>(null);
+  const [activeLocation, setActiveLocation] = useState<Location | null>(null);
+
   return (
     <div style={{ height: '500px', width: '100%' }}>
-    <MapContainer
-      center={[48.8566, 2.3522]} // Початкова позиція карти (Paris)
-      zoom={6}
-      style={{ height: '100%', width: '100%' , marginBottom: 0}}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors"
-      />
-
-      {locations.map((loc) => (
-        <Marker
-          key={loc.id}
-          position={loc.position}
-          icon={goldIcon}
-          eventHandlers={{
-            click: () => {
-              setActiveLocation(loc);
-            },
-          }}
-        >
-          <Popup>
-            <strong>{loc.name}</strong>
-            <br />
-            {loc.address}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-    {activeLocation && (
-        <div style={{paddingTop: 20}}>
+      <MapContainer
+        center={[48.8566, 2.3522]} // Початкова позиція карти (Paris)
+        zoom={6}
+        style={{ height: '100%', width: '100%', marginBottom: 0 }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors"
+        />
+        {locations.map(loc => (
+          <Marker
+            key={loc.id}
+            position={loc.position}
+            icon={goldIcon}
+            eventHandlers={{
+              click: () => {
+                setActiveLocation(loc);
+              },
+            }}
+          >
+            <Popup>
+              <strong>{loc.name}</strong>
+              <br />
+              {loc.address}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+      {activeLocation && (
+        <div style={{ paddingTop: 20 }}>
           <h2>Active Location Details</h2>
           <p>Name: {activeLocation.name}</p>
           <p>Address: {activeLocation.address}</p>
@@ -59,6 +59,6 @@ const InteractiveMap: React.FC = () => {
       )}
     </div>
   );
-}
+};
 
-export default InteractiveMap
+export default InteractiveMap;
